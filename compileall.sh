75208547bf09b7a566e8c001d4ba674f6f484a31
Jáchym Toušek <enumag@gmail.com>
2019-05-26 19:35:54 +0200

    Improve compileall.sh

--- compileall.sh
+++ compileall.sh
@@ -1,16 +1,16 @@
-cargo run onikakushi 5.2.2f1 win
-cargo run onikakushi 5.2.2f1 unix
-cargo run watanagashi 5.2.2f1 win
-cargo run watanagashi 5.2.2f1 unix
-cargo run tatarigoroshi 5.4.0f1 win
-cargo run tatarigoroshi 5.4.0f1 unix
-cargo run tatarigoroshi 5.3.5f1 win
-cargo run tatarigoroshi 5.3.4p1 unix
-cargo run himatsubushi 5.4.1f1 win
-cargo run himatsubushi 5.4.1f1 unix
-cargo run meakashi 5.5.3p3 win
-cargo run meakashi 5.5.3p3 unix
-cargo run meakashi 5.5.3p1 win
-cargo run meakashi 5.5.3p1 unix
-cargo run tsumihoroboshi 5.5.3p3 win
+cargo run onikakushi 5.2.2f1 win && \
+cargo run onikakushi 5.2.2f1 unix && \
+cargo run watanagashi 5.2.2f1 win && \
+cargo run watanagashi 5.2.2f1 unix && \
+cargo run tatarigoroshi 5.4.0f1 win && \
+cargo run tatarigoroshi 5.4.0f1 unix && \
+cargo run tatarigoroshi 5.3.5f1 win && \
+cargo run tatarigoroshi 5.3.4p1 unix && \
+cargo run himatsubushi 5.4.1f1 win && \
+cargo run himatsubushi 5.4.1f1 unix && \
+cargo run meakashi 5.5.3p3 win && \
+cargo run meakashi 5.5.3p3 unix && \
+cargo run meakashi 5.5.3p1 win && \
+cargo run meakashi 5.5.3p1 unix && \
+cargo run tsumihoroboshi 5.5.3p3 win && \
 cargo run tsumihoroboshi 5.5.3p3 unix
