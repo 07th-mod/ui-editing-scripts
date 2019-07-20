58f0f6ebd9e02da81484a1e75948cd6139bf512b
Jáchym Toušek <enumag@gmail.com>
2019-07-20 15:13:18 +0200

    Add Minagoroshi support

--- compileall.sh
+++ compileall.sh
@@ -13,4 +13,6 @@ cargo run meakashi 5.5.3p3 unix && \
 cargo run meakashi 5.5.3p1 win && \
 cargo run meakashi 5.5.3p1 unix && \
 cargo run tsumihoroboshi 5.5.3p3 win && \
-cargo run tsumihoroboshi 5.5.3p3 unix
+cargo run tsumihoroboshi 5.5.3p3 unix && \
+cargo run minagoroshi 5.6.7f1 win && \
+cargo run minagoroshi 5.6.7f1 unix
